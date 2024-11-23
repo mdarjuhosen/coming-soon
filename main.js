2b3219d4b7b1bbf34cf67fe75b225f97062f3d9b
arju <mdarjohosen55@gmail.com>
2024-11-23 20:27:19 +0600

    coming-soon

---
+++ main.js
@@ -0,0 +1,18 @@
+var countdown = document.querySelector('.countdown');
+var x = setInterval(function(){
+    var launchDate = new Date('Mar 31, 2025 10:00:00').getTime();
+    var now = new Date().getTime();
+    var distance = launchDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+    countdown.innerHTML = `
+    <div>${days}<span>Days</span></div>
+    <div>${hours}<span>Hours</span></div>
+    <div>${mins}<span>Mins</span></div>
+    <div>${seconds}<span>Seconds</span></div>
+    `;
+})
